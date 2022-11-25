19cc31972d50df99d8e3d71713abf43887a1c0f9
John O'Halloran <john.ohalloran@telus.com>
2022-11-25 00:36:52 -0500

    added index2.js, iss_promised.js

---
+++ index2.js
@@ -0,0 +1,19 @@
+const {nextISSTimesForMyLocation} = require('./iss_promised');
+const log = console.log;
+
+const printPassTimes = passTimes => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    log("It didn't work: ", error.message);
+  });
